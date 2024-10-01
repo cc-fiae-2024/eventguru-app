b43f06b82e994e7303d1846a8b812689e402f823
André Jaenisch <andre.jaenisch@posteo.de>
2024-10-01 15:24:14 +0200

    feat(lang): provide translation files
    This is localising the register template in German and English.
    It's a good habit to start localising early on as this eases the job
    down the line while not taking up too much effort.
    
    The choice was between messages.php and de.json. Personally I would
    prefer JSON files as most SaaS for translating can handle them easier
    but PHP files allow for running code. I don't know whether we will need
    its flexibility, but I want to leave that door open right now.
    
    All translatable strings must start with 'messages.' to find the correct
    file.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ lang/de/messages.php
@@ -0,0 +1,18 @@
+<?php
+    return [
+        'register' => [
+            'form' => [
+                'email' => 'E-Mail',
+                'name' => 'Name',
+                'password' => 'Passwort',
+                'password_confirmation' => 'Passwort bestätigen',
+                'signin' => 'Du hast bereits ein Konto? :signin',
+                'signup' => "Los geht's!",
+            ],
+            'headline' => 'Konto erstellen',
+            'link' => [
+                'login' => 'Anmelden'
+            ],
+            'subtitle' => 'Beginne deine Momente'
+        ],
+    ];
