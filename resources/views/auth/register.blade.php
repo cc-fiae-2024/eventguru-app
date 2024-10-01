20884538386c00c1a561735db295c0329989d338
André Jaenisch <andre.jaenisch@posteo.de>
2024-10-01 15:21:05 +0200

    refactor: rewrite the register template
    This is more in line with what the Figma design envisioned.
    I prepared the template for translations. You can update your `.env`
    file for German strings going forward.
    
    https://laravel.com/docs/11.x/localization#configuring-the-locale
    
    It's unfortunate that we are using TailwindCSS as this carries with it
    rulesets using `!important` which cannot be easily overwritten further.
    I'd know how to do that in (S)CSS but not in TailwindCSS.
    
    So instead I found creative workarounds.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,35 +1,42 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            <div class="offset-sm-3 col-6 text-center">
+                <x-authentication-card-logo />
+                <h1 class="display-2">{{ __('messages.register.headline') }}</h1>
+                <p>{{ __('messages.register.subtitle') }}</p>
+            </div>
         </x-slot>
 
         <x-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('register') }}">
+        <form method="POST" action="{{ route('register') }}" class="offset-sm-3 col-6">
             @csrf
 
             <div>
-                <x-label for="name" value="{{ __('Name') }}" />
+                <x-label for="name" value="{{ __('messages.register.form.name') }}" />
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
             <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
+                <x-label for="email" value="{{ __('messages.register.form.email') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             </div>
 
             <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
+                <x-label for="password" value="{{ __('messages.register.form.password') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
+            {{-- TODO: Figure out how to turn off password confirmation
             <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-label for="password_confirmation" value="{{ __('messages.register.form.password') }}" />
                 <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
+            --}}
 
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                {{-- TODO: Check whether we actually need a Terms and Privacy Policy Feature
                 <div class="mt-4">
                     <x-label for="terms">
                         <div class="flex items-center">
@@ -44,16 +51,23 @@
                         </div>
                     </x-label>
                 </div>
+                --}}
             @endif
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ms-4">
-                    {{ __('Register') }}
+            <div class="row mt-4">
+                {{-- Compensate for !important padding on x-button with max-w --}}
+                <x-button class="col-12 mx-auto justify-center border-0 bg-primary normal-case tracking-normal !max-w-[calc(100%-1.5rem)]">
+                    {{ __('messages.register.form.signup') }}
                 </x-button>
+            </div>
+            {{-- TODO: Add Social Login --}}
+
+            <div class="row mt-4">
+                <p class="col-12 text-center">
+                    {!! __('messages.register.form.signin', [
+                        'signin' => '<a class="underline text-sm text-primary hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="'.route('login').'">'.__('messages.register.link.login').'</a>'
+                    ]) !!}
+                </p>
             </div>
         </form>
     </x-authentication-card>
