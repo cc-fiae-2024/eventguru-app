832663f87982298f22fce6f8d332bb415561e3ec
André Jaenisch <andre.jaenisch@posteo.de>
2024-10-01 15:21:05 +0200

    refactor: rewrite the register template
    This is more in line with what the Figma design envisioned.
    I prepared the template for translations. You can update your `.env`
    file for German strings going forward.
    
    https://laravel.com/docs/11.x/localization#configuring-the-locale
    
    It's unfortunate that we are using TailwindCSS as this carries with it
    rulesets using `!important` which cannot be easily overwritten further.
    I'd know how to do that in (S)CSS but not in TailwindCSS.
    
    So instead I found creative workarounds.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,9 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-    <div>
+<div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <header class="row">
         {{ $logo }}
-    </div>
+    </header>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+    <div class="row bg-white dark:!bg-gray-800">
         {{ $slot }}
     </div>
 </div>
