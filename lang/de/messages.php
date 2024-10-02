66c566303a1339bb0064b338b99dde1dcb33bed0
André Jaenisch <andre.jaenisch@posteo.de>
2024-10-02 11:01:59 +0200

    refactor: include password confirmation and tos
    After talking to Product Owner it became clear that we do want to keep
    password confirmation. Legal requires a checkbox for terms of service.
    
    On top we have another checkbox for sending out newsletter.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- lang/de/messages.php
+++ lang/de/messages.php
@@ -4,15 +4,18 @@
             'form' => [
                 'email' => 'E-Mail',
                 'name' => 'Name',
+                'newsletter' => 'Ich akzeptiere den Newsletter von EventGuru',
                 'password' => 'Passwort',
                 'password_confirmation' => 'Passwort bestätigen',
                 'signin' => 'Du hast bereits ein Konto? :signin',
                 'signup' => "Los geht's!",
+                'tos' => 'Ich akzeptiere die :terms_of_service',
             ],
             'headline' => 'Konto erstellen',
             'link' => [
-                'login' => 'Anmelden'
+                'login' => 'Anmelden',
+                'tos' => 'AGBs',
             ],
-            'subtitle' => 'Beginne deine Momente'
+            'subtitle' => 'Beginne deine Momente',
         ],
     ];
