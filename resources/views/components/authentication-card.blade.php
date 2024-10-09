c51ba67e1f67852db2543413c5a7f109737ae7fb
Yusef Aslan <gurutosh@web.de>
2024-10-09 13:34:33 +0200

    Design Fix

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,12 +1,12 @@
-<div class="min-h-screen bg-gray-100 dark:bg-gray-900 grid place-items-center">
-   <div class="col-12 col-xl-2">
-    <header>
-        {{ $logo }}
-    </header>
-
-    <div class="bg-white dark:!bg-gray-800 px-4 py-4">
-        {{ $slot }}
-    </div>
-   </div>
+<div class="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-700 dark:text-gray-200 grid place-items-center">
+    <div class="col-12 col-xl-2">
+     <header>
+         {{ $logo }}
+     </header>
  
-</div>
+     <div class="bg-gray-200 dark:!bg-gray-800 text-gray-700 dark:text-gray-200 px-4 py-4">
+         {{ $slot }}
+     </div>
+    </div>
+  
+ </div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         @livewireStyles
     </head>
     <body>
-        <div class="dark:bg-gray-900 font-sans text-gray-900 dark:text-gray-100 antialiased">
+        <div class="font-sans antialiased">
             {{ $slot }}
         </div>
 
