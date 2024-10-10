2a11d4a988182283cfdd3a30a03f846fce4b84b6
André Jaenisch <andre.jaenisch@posteo.de>
2024-10-10 18:24:39 +0200

    feat: replace footer
    We now dynamically compute the year. I added padding all around to have
    a common distance from the edge of the device.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- resources/views/components/event/list.blade.php
+++ resources/views/components/event/list.blade.php
@@ -1,6 +1,6 @@
 @props(['events' => []])
 
-<ul role="list" class="p-0 mx-auto mt-20 text-gray-200 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4">
+<ul role="list" class="px-6 lg:px-8 py-0 mx-auto mt-20 text-gray-200 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4">
     @foreach($events as $event)
         <li>
             <x-event.card :event="$event"></x-event.card>
