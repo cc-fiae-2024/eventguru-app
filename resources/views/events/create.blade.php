0d2acd4ef337dff8b128128cffa3647a138ed2e2
André Jaenisch <andre.jaenisch@posteo.de>
2024-10-16 16:08:35 +0200

    feat: add event validation
    At least for the inputs we already have.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,27 +4,33 @@
         <div class="col-10 col-lg-4 mx-auto">
             <h1>{{  __('messages.events.heading.create') }}</h1>
 
+            <x-validation-errors class="mb-4" />
+
             <form method="POST" action="{{ route('events.store') }}">
                 @csrf
 
                 <div>
                     <x-label for="title" value="{{ __('messages.events.form.title') }}" />
-                    <x-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')" required autofocus />
+                    <x-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')" min="3" max="255" required autofocus />
+                    <div>@error('title') <span class="text-red-600 dark:text-red-400">{{ $message }}</span> @enderror</div>
                 </div>
 
                 <div class="mt-4">
                     <x-label for="description" value="{{ __('messages.events.form.description') }}" />
-                    <x-textarea id="description" class="block mt-1 w-full" type="text" name="description" :value="old('description')" required />
+                    <x-textarea id="description" class="block mt-1 w-full" type="text" name="description" :value="old('description')" min="3" max="4096" required />
+                    <div>@error('description') <span class="text-red-600 dark:text-red-400">{{ $message }}</span> @enderror</div>
                 </div>
 
                 <div class="mt-4">
                     <x-label for="starts_at" value="{{ __('messages.events.form.starts_at') }}" />
-                    <x-input id="starts_at" class="block mt-1 w-full" type="datetime-local" name="starts_at" :value="old('starts_at')" required />
+                    <x-input id="starts_at" class="block mt-1 w-full" type="datetime-local" name="starts_at" :value="old('starts_at')" min="{{ \Carbon\Carbon::now()->format('Y-m-d\TH:i') }}" value="{{ \Carbon\Carbon::now()->format('Y-m-d\TH:i') }}" required />
+                    <div>@error('starts_at') <span class="text-red-600 dark:text-red-400">{{ $message }}</span> @enderror</div>
                 </div>
 
                 <div class="mt-4">
                     <x-label for="ends_at" value="{{ __('messages.events.form.ends_at') }}" />
-                    <x-input id="ends_at" class="block mt-1 w-full" type="datetime-local" name="ends_at" :value="old('ends_at')" required />
+                    <x-input id="ends_at" class="block mt-1 w-full" type="datetime-local" name="ends_at" :value="old('ends_at')" min="{{ \Carbon\Carbon::now()->format('Y-m-d\TH:i') }}" value="{{ \Carbon\Carbon::now()->addDay(1)->format('Y-m-d\TH:i') }}" required />
+                    <div>@error('ends_at') <span class="text-red-600 dark:text-red-400">{{ $message }}</span> @enderror</div>
                 </div>
 
                 <div class="mt-4">
