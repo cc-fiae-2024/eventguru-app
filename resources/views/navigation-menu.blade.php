b4cc8d6f81782e0d234cf2c1eb5a4800391a78a3
apalgen <apalgen@gmail.com>
2024-10-18 14:40:22 +0200

    Abstand im Header vergrößert

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -2,15 +2,15 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
+            <div class="flex items-center">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center mr-5">
                     <a href="{{ route('dashboard') }}">
                         <x-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
 
-                <h1>{{ __('messages.dashboard.headline') }}</h1>
+                <h1 class="mb-0">{{ __('messages.dashboard.headline') }}</h1>
             </div>
         </div>
     </div>
